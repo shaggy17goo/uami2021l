a995d0989e37e7cdd844d9177cb87324030ec2b4
Paweł Mikołaj Gryka <p.gryka@stud.elka.pw.edu.pl>
2021-04-13 02:01:25 +0200

    Add template for Appointments Data microservice

---
+++ AppointmentsData/AppointmentsData.Domain/PatientAggregate/Appointment.cs
@@ -0,0 +1,27 @@
+﻿namespace AppointmentsData.Domain.PatientAggregate
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Appointment
+    {
+        public int appointmentId { get; set; }
+        public int doctorId { get; set; }
+        public int patientId { get; set; }
+        public DateTime dateOfAppointment { get; set; }
+        public string description { get; set; }
+
+
+        public Appointment(int appointmentId, int doctorId, int patientId, DateTime dateOfAppointment,
+            string description)
+        {
+            this.appointmentId = appointmentId;
+            this.doctorId = doctorId;
+            this.patientId = patientId;
+            this.dateOfAppointment = dateOfAppointment;
+            this.description = description;
+        }
+    }
+}
---
+++ AppointmentsData/AppointmentsData.Infrastructure/Constants.cs
@@ -0,0 +1,8 @@
+﻿
+namespace AppointmentsData.Infrastructure
+{
+    internal class Constants
+    {
+        public const string connectionString = @"Server=tcp:uami.database.windows.net,1433;Initial Catalog=appointmentsDB;Persist Security Info=False;User ID=shaggy17goo;Password={your_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+    }
+}
