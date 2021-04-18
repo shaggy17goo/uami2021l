51e21d907dda46b5f46ac908efddebb52e865ab1
Michał Wawrzyńczak <m.wawrzynczak@stud.elka.pw.edu.pl>
2021-04-19 00:00:39 +0200

    Remove appointmentId from AddAppointmentCommand

--- DoctorsApplicationMicroservice/DoctorsApplicationMicroservice.Web/Application/Commands/Commands/AddAppointmentCommand.cs
+++ DoctorsApplicationMicroservice/DoctorsApplicationMicroservice.Web/Application/Commands/Commands/AddAppointmentCommand.cs
@@ -5,7 +5,6 @@
     //wykorzystujemy interfejs wskaźnikowy ICommand do jawnego wskazania, że dana klasa jest komendą
     public class AddAppointmentCommand : ICommand
     {
-        public int AppointmentId { get; set; }
         public int DoctorId { get; set; }
         public int PatientId { get; set; }
         public DateTime DateOfAppointment { get; set; }
