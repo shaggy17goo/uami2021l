ffb7f2eb881dd4b3d4637272551da68e753af577
Michał Wawrzyńczak <m.wawrzynczak@stud.elka.pw.edu.pl>
2021-04-18 21:34:35 +0200

    Edit PostData method

--- DoctorsApplicationMicroservice/DoctorsApplicationMicroservice.Web/Application/DataServiceClients/GenericServiceClient.cs
+++ DoctorsApplicationMicroservice/DoctorsApplicationMicroservice.Web/Application/DataServiceClients/GenericServiceClient.cs
@@ -3,7 +3,6 @@ using System.Net.Http;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
-using JsonSerializer = System.Text.Json.JsonSerializer;
 
 namespace DoctorsApplicationMicroservice.Web.Application.DataServiceClients
 {
