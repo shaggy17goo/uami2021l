15fb86fb6d02b3116a2829b21cb8b10fa38bdb69
Izabela Wierzejska <i.wierzejska@stud.elka.pw.edu.pl>
2021-05-16 20:14:29 +0200

    Added views for Doctors Web Application

---
+++ DoctorApplication/ZsutPwPatterns.WindowsApplication/View/Pages/Patients.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+
+using ZsutPw.Patterns.WindowsApplication.Controller;
+using ZsutPw.Patterns.WindowsApplication.Model;
+using ZsutPw.Patterns.WindowsApplication.Utilities;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace ZsutPw.Patterns.WindowsApplication.View
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Patients : Page
+    {
+
+        public IData Model { get; private set; }
+
+        public IController Controller { get; private set; }
+
+        public Patients()
+        {
+            this.InitializeComponent();
+
+            IEventDispatcher dispatcher = new EventDispatcher() as IEventDispatcher;
+
+            this.Controller = ControllerFactory.GetController(dispatcher);
+
+            this.Model = this.Controller.Model as IData;
+
+            this.DataContext = this.Controller;
+        }
+    }
+}
