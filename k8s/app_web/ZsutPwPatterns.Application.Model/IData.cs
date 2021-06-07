b6093fc67be0113fe87988cc29882545fbfba608
Jakub Olszewski <j.olszewski.2@stud.elka.pw.edu.pl>
2021-06-07 18:22:55 +0200

    .

---
+++ k8s/app_web/ZsutPwPatterns.Application.Model/IData.cs
@@ -0,0 +1,49 @@
+﻿//===============================================================================
+//
+// PlaZa System Platform
+//
+//===============================================================================
+//
+// Warsaw University of Technology
+// Computer Networks and Services Division
+// Copyright © 2020 PlaZa Creators
+// All rights reserved.
+//
+//===============================================================================
+
+namespace ZsutPw.Patterns.Application.Model
+{
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+  using System.Text;
+  using System.Threading.Tasks;
+
+  using System.ComponentModel;
+    using ZsutPw.Patterns.Application.Model.Data;
+
+    public interface IData : INotifyPropertyChanged
+  {
+    string SearchText { get; set; }
+    string VisitDate { get; set; }
+    string Pesel { get; set; }
+        string Name { get; set; }
+        string Surname { get; set; }
+        string Sex { get; set; }
+        string BirthDate { get; set; }
+        string City { get; set; }
+        string Street { get; set; }
+        string HouseNr { get; set; }
+        string DoctorId { get; set; }
+        string PatientId { get; set; }
+        string DateOfAppointment { get; set; }
+        string Description { get; set; }
+        PatientDto PatientById { get; }
+    List<PatientsShortDto> PatientsByDoctorId { get; }
+    PatientsShortDto SelectedPatientsByDoctorId { get; set; }
+    List<AppointmentsWithPatientNameDto> AppointmentsByDoctorIdAndData { get; }
+    AppointmentsWithPatientNameDto SelectedAppointmentByDoctorIdAndData { get; set; }
+    List<AppointmentsWithPatientNameDto> AppointmentsByDoctorId { get; }
+    AppointmentsWithPatientNameDto SelectedAppointmentByDoctorId{ get; set; }
+    }
+}
---
+++ k8s/app_web/ZsutPwPatterns.Application.Model/Model_Operations.cs
@@ -0,0 +1,156 @@
+﻿//===============================================================================
+//
+// PlaZa System Platform
+//
+//===============================================================================
+//
+// Warsaw University of Technology
+// Computer Networks and Services Division
+// Copyright © 2020 PlaZa Creators
+// All rights reserved.
+//
+//===============================================================================
+
+namespace ZsutPw.Patterns.Application.Model
+{
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+  using System.Text;
+  using System.Threading.Tasks;
+    using ZsutPw.Patterns.Application.Model.Data;
+  public partial class Model : IOperations
+  {
+    public void LoadAppointmentsByDoctorId()
+        {
+            Task.Run(() => LoadAppointmentsByDoctorIdTask());
+        }
+    public void LoadAppointmentsByDoctorIdAndDate()
+        {
+            Task.Run(() => LoadAppointmentsByDoctorIdAndDataTask());
+        }
+    public void LoadPatientsByDoctorId()
+        {
+            Task.Run(() => LoadPatientsByDoctorIdTask());
+        }
+    public void LoadPatientById()
+        {
+            Task.Run(() => LoadPatientByIdTask());
+        }
+    public void DeleteAppointment()
+        {
+            Task.Run(() => DeleteAppointmentTask());
+        }
+    public void AddAppointment()
+        {
+            Task.Run(() => AddAppointmentTask());
+        }
+    public void AddPatient()
+        {
+            Task.Run(() => AddPatientTask());
+        }
+
+        private void AddPatientTask()
+        {
+            var networkClient = NetworkClientFactory.GetNetworkClient();
+
+            try
+            {
+                networkClient.AddPatient(Pesel, Name, Surname, Sex, BirthDate, City, Street, HouseNr);
+
+            }
+            catch (Exception e)
+            {
+                string mess = e.Message;
+            }
+        }
+        private void AddAppointmentTask()
+        {
+            var networkClient = NetworkClientFactory.GetNetworkClient();
+
+            try
+            {
+                networkClient.AddAppointment(DoctorId, PatientId, DateOfAppointment, Description);
+
+            }
+            catch (Exception e)
+            {
+                string mess = e.Message;
+            }
+        }
+
+        private void DeleteAppointmentTask()
+        {
+            var networkClient = NetworkClientFactory.GetNetworkClient();
+
+            try
+            {
+                networkClient.DeleteAppointment(SearchText);
+
+            }
+            catch (Exception e)
+            {
+                string mess = e.Message;
+            }
+        }
+
+        private void LoadAppointmentsByDoctorIdTask()
+        {
+            var networkClient = NetworkClientFactory.GetNetworkClient();
+
+            try
+            {
+                var appointments = networkClient.GetAppointmentsByDoctorId(SearchText);
+
+                AppointmentsByDoctorId = appointments.ToList();
+            }
+            catch (Exception)
+            {
+            }
+        }
+    private void LoadAppointmentsByDoctorIdAndDataTask()
+        {
+            var networkClient = NetworkClientFactory.GetNetworkClient();
+
+            try
+            {
+                var appointments = networkClient.GetAppointmentsByDoctorIdAndData(SearchText, VisitDate);
+
+                AppointmentsByDoctorIdAndData = appointments.ToList();
+            }
+            catch (Exception)
+            {
+            }
+        }
+    private void LoadPatientsByDoctorIdTask()
+        {
+            var networkClient = NetworkClientFactory.GetNetworkClient();
+
+            try
+            {
+                var patients = networkClient.GetPatientsByDoctorId(SearchText);
+
+                PatientsByDoctorId = patients.ToList();
+            }
+            catch (Exception e)
+            {
+                string ExceptionMess = e.Message;
+
+            }
+        }
+    private void LoadPatientByIdTask()
+        {
+            var networkClient = NetworkClientFactory.GetNetworkClient();
+
+            try
+            {
+                var patient = networkClient.GetPatientById(SearchText);
+
+                PatientById = patient;
+            }
+            catch (Exception)
+            {
+            }
+        }
+    }
+}
